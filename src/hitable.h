a411c4727b11f876c70943bcdb2cef628da9fd4d
unknown <vaselas99@gmail.com>
2020-12-28 09:50:18 +0200

    hitables

---
+++ src/hitable.h
@@ -0,0 +1,97 @@
+#ifndef HITABLE_H
+#define HITABLE_H
+#include "tools.h"
+#include "ray.h"
+
+typedef struct HitRecord
+{
+  f32 t;
+  vec3 p;
+  vec3 normal;
+}HitRecord;
+
+typedef enum HitableType
+{
+  SPHERE = 1,
+  TRIANGLE = 2,
+}HitableType;
+typedef struct Sphere
+{
+  vec3 center;
+  f32 radius;
+}Sphere;
+
+static i32 
+sphere_hit(Sphere s, Ray r, f32 t_min, f32 t_max, HitRecord *rec)
+{
+  vec3 oc = vec3_sub(r.o, s.center);
+  f32 a = vec3_dot(r.d, r.d);
+  f32 b = vec3_dot(oc, r.d);
+  f32 c = vec3_dot(oc,oc) - s.radius * s.radius;
+
+  f32 d = b*b - a*c;
+  if (d > 0)
+  {
+    f32 temp = (-b - sqrt(b*b - a*c))/a;
+    if (temp < t_max && temp > t_min)
+    {
+      rec->t = temp;
+      rec->p = ray_point_at(r, rec->t);
+      rec->normal = vec3_normalize(vec3_divf(vec3_sub(rec->p, s.center), s.radius));
+      return 1;
+    }
+    temp = (-b + sqrt(b*b - 4*a*c))/a;
+    if (temp < t_max && temp > t_min)
+    {
+      rec->t = temp;
+      rec->p = ray_point_at(r, rec->t);
+      rec->normal = vec3_normalize(vec3_divf(vec3_sub(rec->p, s.center), s.radius));
+      return 1;
+    }
+  }
+  return 0;
+}
+
+typedef struct Hitable
+{
+  union
+  {
+    Sphere s;
+  };
+  HitableType type;  
+}Hitable;
+
+static i32 
+hitable_hit(Hitable *hitable, Ray r, f32 t_min, f32 t_max, HitRecord * rec)
+{
+  if (hitable->type == SPHERE)
+    return sphere_hit(hitable->s, r, t_min, t_max, rec);
+  return 0;
+}
+
+typedef struct HitableList
+{
+  Hitable **list;
+  u32 list_size;
+}HitableList;
+
+static i32 
+hitable_list_hit(HitableList *hl, Ray r, f32 t_min, f32 t_max, HitRecord *rec)
+{
+  HitRecord temp_rec;
+  i32 hit_anything = 0;
+  //we need f64 to not have rounding errors
+  f64 closest_so_far = t_max;
+  for (u32 i = 0; i < hl->list_size;++i)
+  {
+    if (hitable_hit(hl->list[i],r,t_min,closest_so_far, &temp_rec))
+    {
+      hit_anything = 1;
+      closest_so_far = temp_rec.t;
+      *rec = temp_rec;
+    }
+  }
+  return hit_anything;
+}
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,39 +1,24 @@
 #include "tools.h"
 #include "ray.h"
+#include "hitable.h"
 
 static u32 window_width = 800;
 static u32 window_height= 400;
 
-static f32 
-hit_sphere(vec3 center, f32 radius, Ray r)
+static vec3 color(Ray r, HitableList *world)
 {
-  vec3 oc = vec3_sub(r.o, center);
-  f32 a = vec3_dot(r.d, r.d);
-  f32 b = 2.f * vec3_dot(oc, r.d);
-  f32 c = vec3_dot(oc,oc) - radius * radius;
-
-  f32 d = b*b - 4*a*c;
-  if (d < 0)
-    return -1.f;
-  else 
-    return (-b - sqrt(d)) / (2.f * a);
-}
-
-static vec3
-color(Ray r)
-{
-  f32 t = hit_sphere(v3(0,0,-1), 0.5, r);
-  if (t > 0.f)
+  HitRecord rec;
+  if (hitable_list_hit(world, r, 0.f, 10000.f, &rec))
   {
-    vec3 N = vec3_normalize(vec3_sub(ray_point_at(r, t), v3(0,0,-1)));
-    return vec3_mulf(v3(N.x + 1, N.y + 1, N.z + 1), 0.5);
+    //rec.normal = vec3_normalize(rec.normal);
+    return vec3_mulf(v3(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1), 0.5);
   }
   vec3 unit_direction = vec3_normalize(r.d);
-  t = 0.5f * (unit_direction.y + 1.f);
+  f32 t = 0.5f * (unit_direction.y + 1.f);
   return vec3_add(vec3_mulf(v3(1.f,1.f,1.f),1.f - t), vec3_mulf(v3(0.5, 0.4f,0.7f), t));
-}
 
-static int 
+}
+static i32 
 main(void)
 {
   f32 *pixels = ALLOC(sizeof(f32) * window_width * window_height * 3);
@@ -42,6 +27,17 @@ main(void)
   vec3 horizontal = v3(4.f,0.f,0.f);
   vec3 vertical = v3(0.f,2.f,0.f);
   vec3 origin = v3(0.f,0.f,0.f);
+  Hitable *list[2];
+  list[0] = ALLOC(sizeof(Hitable));
+  list[0]->type = SPHERE;
+  list[0]->s = (Sphere){v3(0,0,-1), 0.5f};
+  list[1] = ALLOC(sizeof(Hitable));
+  list[1]->type = SPHERE;
+  list[1]->s = (Sphere){v3(0,-100.5f,-1.f), 100.f};
+  HitableList hlist;
+  hlist.list = list;
+  hlist.list_size = 2;
+  printf("tracing rays\n");
   for (int i = 0; i < window_width * window_height; ++i)
   {
       u32 x = i % window_width;
@@ -49,10 +45,12 @@ main(void)
       f32 u = x / (f32)window_width;
       f32 v = y / (f32)window_height;
       Ray r = ray_init(origin, vec3_add(lower_left_corner, vec3_add(vec3_mulf(horizontal, u), vec3_mulf(vertical, v))));
-      vec3 col = color(r);
+      vec3 col = color(r, &hlist);
       pixels[3*i] = col.x;
       pixels[3*i+1] = col.y;
       pixels[3*i+2] = col.z;
   }
+  printf("writing to disk\n");
   ppm_save_pixels(window_width, window_height, pixels);
+  printf("finished\n");
 }
